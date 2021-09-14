bbe04ff53ba05f4aeee65961b6f93044ee7b188e
Josh Spicer <hello@joshspicer.com>
2021-09-14 12:25:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
+
 #-----------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See LICENSE in the project root for license information.
 #-----------------------------------------------------------------------------------------
 
 from flask import Flask
+import pandas as pd
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    df = pd.DataFrame
+    data = {}
+    arr = pd.DataFrame(data)
     return app.send_static_file("index.html")
